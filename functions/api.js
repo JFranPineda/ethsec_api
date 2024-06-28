f24ca3cd3683db027d995ef200b94a716ba818ac
JFranPineda <jpinedas@uni.pe>
2024-06-28 17:03:15 -0500

    added netlify dependencies

---
+++ functions/api.js
@@ -0,0 +1,30 @@
+import express, { json } from 'express'
+import { productsRouter } from '../routes/products.js'
+import { clientsRouter } from '../routes/clients.js'
+import { sellersRouter } from '../routes/sellers.js'
+import { moneyCatalogRouter } from '../routes/money_catalog.js'
+import { billingsRouter } from '../routes/billings.js'
+import { corsMiddleware } from '../middlewares/cors.js'
+import serverless from 'serverless-http'
+
+const app = express()
+
+app.use(json())
+app.use(corsMiddleware())
+app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
+
+app.use('/products', productsRouter)
+app.use('/clients', clientsRouter)
+app.use('/sellers', sellersRouter)
+app.use('/money_catalog', moneyCatalogRouter)
+app.use('/billings', billingsRouter)
+
+const PORT = process.env.PORT ?? 3000
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
+
+exports.handler = async (event, context) => {
+  return serverless(app)
+}
