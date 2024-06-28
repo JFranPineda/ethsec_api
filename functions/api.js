bb2c88c572f37b2343a7a1043dfcbba2a8fd183b
JFranPineda <jpinedas@uni.pe>
2024-06-28 18:01:56 -0500

    Fixing prod dirname and filename

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,5 @@
 import express, { json } from 'express'
 import path from 'path'
-import { fileURLToPath } from 'url'
 import { productsRouter } from '../routes/products.js'
 import { clientsRouter } from '../routes/clients.js'
 import { sellersRouter } from '../routes/sellers.js'
@@ -8,9 +7,6 @@ import { moneyCatalogRouter } from '../routes/money_catalog.js'
 import { billingsRouter } from '../routes/billings.js'
 import { corsMiddleware } from '../middlewares/cors.js'
 import serverless from 'serverless-http'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 const app = express()
 
